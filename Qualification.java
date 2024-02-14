1f7027580ccdb6f008ed01fb24f2655390a0bcfa
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-02-14 11:29:10 +0530

    added if-else and flag in the code

--- Qualification.java
+++ Qualification.java
@@ -3,25 +3,51 @@ public class Qualification {
 
 	public static void main(String[] args) {
 
-		int yop = 2022;
-		int qCode = 1;
-		int sCode = 4;
-		float perc = 65.4f;
+		int yop = 2025;
+		int qCode = 5;
+		int sCode = 2;
+		float perc = 66.4f;
+		int flag = 0;
+		
+		
+		if(perc< 50) {
+			System.out.println("Not eligible");			
+		}else if(yop != 2022 && yop != 2023 && yop != 2024) {
+			System.out.println("Not eligible");		
+			
+		}else if(qCode != 1 && qCode != 3 && qCode != 4) {
+			System.out.println("Not eligible");
+			
+		}else if(sCode != 1 && sCode != 2 && sCode != 4) {
+			System.out.println("Not eligible");
+			
+		}else {
+			System.out.println("Eligible");
+		}
+		
 		
 		if(perc < 50) {
-			System.out.println("Not");
+			flag =1;
+			System.out.println("Parcentage not matching");
 		}
 		if(yop != 2022 && yop != 2023 && yop != 2024) {
-			System.out.println("Not");
+			flag = 1;
+			System.out.println("YOP not matching");
 		}
 		if(qCode != 1 && qCode != 3 && qCode != 4) {
-			System.out.println("NOt");
+			flag =1;
+			System.out.println("Qualification not matching");
 		}
 		if(sCode != 1 && sCode != 2 && sCode != 4) {
-			System.out.println("Not");
+			flag = 1;
+			System.out.println("Stream not matching");
+		}
+		
+		if(flag == 0){
+			System.out.println("Eligible");
 		}
 		else {
-			System.out.println("Eligible");
+			System.out.println("Not eligible");
 		}
 		
 	}
