eaf67830455680f2e42a2b3c7f64b4e38f53f471
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-02-21 15:11:23 +0530

    Relational operators

---
+++ RelationalOperator.java
@@ -0,0 +1,20 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+        // check the relation between values or variables
+        // >, >=, <, <=, ==, !=
+        // Result is always boolean(True or False)
+        // mostly used to decision making statements
+        int x = 10;
+        int y = 20;
+        int z = 5;
+        int p = 10;
+        System.out.println(x > y); // False
+        System.out.println(x < y); // True
+        System.out.println(x == z); // False
+        System.out.println(x != y); // True
+        System.out.println(x == p); // True
+        System.out.println(x >= y); // False
+        System.out.println(x <= y); // True
+
+    }
+}
