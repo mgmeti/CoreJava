661cfc334ba860d8b51a1a6205d89c41efe3137d
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-02-28 14:33:53 +0530

    Added multiple comments

--- ArraysExamples.java
+++ ArraysExamples.java
@@ -1,9 +1,17 @@
 public class ArraysExamples {
     public static void main(String[] args) {
         System.out.println("Program Started");
+
+
+        
+        // Both declaration and initialization are combined in one line code
+        // datatype[] arrayName = new datatype[array-size];
+
         int[] arr; // Array Declaration
 
         arr = new int[5];// Array initialization
+
+         // int[] arr = new int[5];
 
         arr[0] = 1;
         arr[1] = 2;
@@ -11,6 +19,8 @@ public class ArraysExamples {
         arr[3] = 4;
         arr[4] = 5;
         // arr[5] = 6;//java.lang.ArrayIndexOutOfBoundsException: 5
+
+         // int[] arr1 = {1, 2, 3, 4, 5};
 
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
@@ -20,3 +30,4 @@ public class ArraysExamples {
     }
 
 }
+
