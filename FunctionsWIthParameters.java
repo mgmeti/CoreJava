1ce263fb6d6cd3eec3244531ddc06ffd4d2d0fea
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-02-29 15:00:14 +0530

    Passing value and copying variable value to the function parameter.

--- FunctionsWIthParameters.java
+++ FunctionsWIthParameters.java
@@ -17,12 +17,18 @@ public class FunctionsWithParameters {
 
     public static void main(String[] args) {
         System.out.println("Main method started");
+        // Passing int value to function parameter
         square(2);
         square(15);
         square(25);
         cube(2);
         cube(15);
         cube(25);
+
+        // Copying x value to a function parameter
+        int x = 10;
+        square(x);
+        cube(x);
 
         System.out.println("Main method ended");
     }
