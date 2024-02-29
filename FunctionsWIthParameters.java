7dcb95044dc3e678141f24bd665ac1558f62a5cb
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-02-29 14:51:49 +0530

    Added cube method

--- FunctionsWIthParameters.java
+++ FunctionsWIthParameters.java
@@ -9,10 +9,20 @@ public class FunctionsWithParameters {
 
     }
 
+    public static void cube(int num) {
+        int res = num * num * num;
+        System.out.println("Cube of " + num + " is " + res);
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Main method started");
+        square(2);
         square(15);
         square(25);
+        cube(2);
+        cube(15);
+        cube(25);
 
         System.out.println("Main method ended");
     }
