559c6e7d01ab065adc4c2515399a13bc5450ba7b
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-03-05 15:05:22 +0530

    Return type functions examples.

--- FunctionsWIthReturnType.java
+++ FunctionsWIthReturnType.java
@@ -1,12 +1,26 @@
 public class FunctionWithReturnType {
 
+    // Method returning String value
     public static String display() {
         System.out.println("Java");
         return "SQL";
     }
 
+    // Method returning variable
+    public static String returnVariable() {
+        String sub = "Linux";
+        return sub;
+    }
+
+    // Method returning variable
+    public static String returnParameter(String name) {
+        return name;
+    }
+
     public static void main(String[] args) {
         System.out.println(display());
+        System.out.println(returnVariable());
+        System.out.println(returnParameter("Rahul"));
     }
 
 }
