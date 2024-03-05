9f8f747a1119191dfd70e98d7dbcc97bb687e1e0
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-03-05 12:51:56 +0530

    Added comments to increase the clean code.

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Practice with the Projects
 # Concepts
 1. Control statements -> if(multiple if's)s, if-else, else-if ladder.
 2. Logic operators -> '&&', '||', '!', '==', '<', '<=', '>', '>='
+
+Note: Clean Code - The that feels like written with care.
+
+
+# Note: Rush will mess things.
--- WhileLoop.java
+++ WhileLoop.java
@@ -1,10 +1,12 @@
 class WhileStatements {
     public static void main(String[] args) {
-
-        // while condition is true keep on executing while loop body stmts
-        // stops when condition becomes false
-        // Note: while loop must have stmt, which makes the condition become false
-        // otherwise while ends in infinite loop
+        /*
+        while condition is true, keep on executing while loop body stmts
+        stops when condition becomes false
+        Note: while loop must have stmt, which makes the condition become false
+        otherwise while ends in infinite loop
+        */
+        // Print tick with number for n times.    
         int n = 3;
 
         while (n > 0) {
@@ -12,12 +14,13 @@ class WhileStatements {
             n--;
         }
 
+        //Find the mid number between i and j.
         int i, j;
         i = 100;
         j = 200;
 
-        while (++i < --j)
-            ;
+        //pre-increment of i, j and check with condition like i < j.
+        while (++i < --j) ; // null statement 
 
         System.out.println("Midpoint: " + i);
 
