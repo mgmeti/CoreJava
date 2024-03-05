83ee1398786b958452fdb638fab0e2ece2badca6
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-03-05 14:59:50 +0530

    Return type function example.

---
+++ FunctionsWIthReturnType.java
@@ -0,0 +1,12 @@
+public class FunctionWithReturnType {
+
+    public static String display() {
+        System.out.println("Java");
+        return "SQL";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(display());
+    }
+
+}
