2e56e2ba7e684745e04cc1b91e8def1d216eedf6
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-03-08 23:00:05 +0530

    volume method added box class.

--- ClassesBasics/BoxDemo3.java
+++ ClassesBasics/BoxDemo3.java
@@ -1,17 +1,20 @@
-class Box {
+class BoxWithMethod {
     double width;
     double height;
     double depth;
+
+    void volume() {
+        System.out.println("Volume " + width * depth * height);
+    }
+
 }
 
-public class BoxDemo2 {
+public class BoxDemo3 {
     public static void main(String[] args) {
 
         // Declare two Box objects
-        Box myBox1 = new Box();
-        Box myBox2 = new Box();
-
-        double vol;
+        BoxWithMethod myBox1 = new BoxWithMethod();
+        BoxWithMethod myBox2 = new BoxWithMethod();
 
         // Assign values to myBox1's instance variables
         myBox1.width = 10;
@@ -23,13 +26,7 @@ public class BoxDemo2 {
         myBox2.height = 6;
         myBox2.depth = 9;
 
-        // Compute volume of first box
-        vol = myBox1.width * myBox1.depth * myBox1.height;
-        System.out.println("Volume is " + vol);
-
-        // Compute volume of second box
-        vol = myBox2.width * myBox2.depth * myBox2.height;
-        System.out.println("Volume is " + vol);
+        myBox1.volume();
+        myBox2.volume();
     }
-
 }
